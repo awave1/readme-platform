47bb3f7f4ed40a39e99968807f13a8c0c5e48f3b
awave1 <artgolovin96@gmail.com>
2018-02-06 01:01:20 -0700

    moved Navbar to separate component

--- app/src/components/App/App.js
+++ app/src/components/App/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
+import Toolbar from '../Toolbar/Toolbar'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">readme</h1>
-        </header>
+        <Toolbar/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
