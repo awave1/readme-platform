5a3371a14edd8060c7b72450040e6e153af6b6a9
awave1 <artgolovin96@gmail.com>
2018-03-07 10:57:59 -0700

    use *.ts for tests

--- server/test/models/user.test.ts
+++ server/test/models/user.test.ts
@@ -1,9 +1,11 @@
-const { chai, assert, should, expect, User } = require('../common')
+import chai from 'chai'
+import User from '../../src/models/User'
+import 'mocha'
 
 describe('user model test', () => {
   it('should create a user object with valid fields', () => {
     const user = new User("first", "last", "username", "email@email.com")
-    chai.expect(user).to.have.property("uuid")
+    chai.expect(user).to.have.property("userId")
     chai.expect(user).to.have.property("fullName")
     chai.expect(user).to.have.property("fullName").equal("first last")
   })
