cb7384b364a0f19c33d92459a787d9c38324715d
awave1 <artgolovin96@gmail.com>
2018-03-17 22:13:00 -0600

    use bootstrap cols to split content on profile page

--- app/src/components/Account/Account.js
+++ app/src/components/Account/Account.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Row, Col } from 'reactstrap'
 import Header from '../Header/Header.js'
 import ProfileCard from '../ProfileCard/ProfileCard.js'
 
@@ -7,7 +8,17 @@ class Account extends Component {
     return(
       <div id="profile-page">
         <Header image='https://78.media.tumblr.com/26f1db79797aaf8577a32e288c5afefd/tumblr_n7yhhvUQtx1st5lhmo1_1280.jpg' lg/>
-        <ProfileCard image='http://placehold.it/100x100?text=Picture'/>
+        <Row>
+          <Col>
+            <ProfileCard image='http://placehold.it/100x100?text=Picture' fitToCol/>
+          </Col>
+          <Col xs="6">
+            {/* Content goes in this column */}
+          </Col>
+          <Col>
+            {/* The rest is here */}
+          </Col>
+        </Row>
       </div>
     )
   }
--- app/src/components/ProfileCard/ProfileCard.js
+++ app/src/components/ProfileCard/ProfileCard.js
@@ -3,25 +3,31 @@ import './ProfileCard.css'
 
 
 class ProfileCard extends Component {
-    render() {
-      return(
-        <div className="profile-card">
-          <div className="profile-thumbnail"
-            style={{
-              backgroundImage: `url(${this.props.image})`
-            }}></div>
 
-          <div className="profile-info">
-            <h2>Test User</h2>
-            <div className="profile-card-info">
-              <p className="profile-info-text"> This is test info </p>
-            </div>
-            <div className="profile-info-social">
-            </div>
+  constructor(props) {
+    super(props)
+    this.inheritWidth = this.props.fitToCol === true ? 'inherit' : '300px'
+  }
+
+  render() {
+    return(
+      <div className="profile-card" style={{width: this.inheritWidth}}>
+        <div className="profile-thumbnail"
+          style={{
+            backgroundImage: `url(${this.props.image})`
+          }}></div>
+
+        <div className="profile-info">
+          <h2>Test User</h2>
+          <div className="profile-card-info">
+            <p className="profile-info-text"> This is test info </p>
+          </div>
+          <div className="profile-info-social">
           </div>
         </div>
-      )
-    }
+      </div>
+    )
+  }
 }
 
 
