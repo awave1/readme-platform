29321cf6ca2d933ff2dcd2f7b01a71e35bc5e9de
awave1 <artgolovin96@gmail.com>
2018-03-23 17:40:04 -0600

    added connection to heroku db

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -14,8 +14,8 @@ class UserController {
   static async createNewUser(body: any) {
     const user = new User(body.first, body.last, body.username, body.email, body.password)
     const query = 'INSERT INTO r_users(uid, first, last, username, email, password) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;'
-    const result = await db.query(query, user.getValues().slice(0, user.getValues().length));
-    return result.rows[0];
+    const result = await db.query(query, user.getValues().slice(0, user.getValues().length))
+    return result.rows[0]
   }
 }
 
--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,11 +1,7 @@
 import { Pool, QueryResult } from 'pg'
 
 const pool = new Pool({
-  user: 'awave',
-  host: 'localhost',
-  database: 'readme',
-  password: 'password',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
 })
 
 export default {
